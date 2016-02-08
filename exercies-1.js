d3518b372cde760bffd48b2e1ff05f3ad30c059d
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-08 13:15:14 -0500

    adding examples

--- exercies-1.js
+++ exercies-1.js
@@ -1,1 +1,33 @@
+/**
+ * For each of the following Array methods,
 
+ * 1. use them in an example
+ * 2. use console.log() to make the code run in the console!
+
+ **/
+
+// .sort()
+
+// .concat()
+
+// .indexOf()
+
+// .split()
+
+// .join()
+
+// .pop()
+
+// .push()
+
+// .slice()
+
+// .splice()
+
+// .shift()
+
+// .unshift()
+
+// .filter()
+
+// .map()
