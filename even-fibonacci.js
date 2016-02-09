465d543037cc9cabc8e4f088345eb4ceea0affb0
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-08 21:01:31 -0500

    completing while loop

--- even-fibonacci.js
+++ even-fibonacci.js
@@ -1,30 +1,33 @@
 // 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 
-// use a while loop?
+// use a for loop?
+// for (var i = 0; i < 4000000 
 // while (condition) {
 //   statement
 // }
 
-
+// create variable to hold sum
+var sum = 0;
 // create a variable for the first number
 var first = 0
 // create a variable for the second number
 var second = 1
 //create a variable that adds the first number and the second number
-var third = first + second;
+var third = first + second
 
 
-while (first < 4000000) { //condition will run if the first number < 4 million
+while (third < 4000000) { //condition will run third < 4 million
 // console.log(first + second);
 
-  third = first + second;
-  first = second;
-  second = third;
+  if  (third % 2 === 0) { // if it is even
+// console.log(third);
+    sum += third;
+  } // closes if statement
 
-  if  (third % 2 === 0) { // if it is even then print
-    console.log(third);
-  }
-
-
+    third = first + second;
+    first = second;
+    second = third;
 
 } // closes while loop
+
+  console.log(sum);
