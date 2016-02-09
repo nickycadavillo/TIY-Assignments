9b981b8f60f9e0c27fc0074e99a8041dae0cb748
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-09 15:48:19 -0500

    getting fiz and buzz but not fizzbuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -10,6 +10,24 @@
 // create a variable for FizzBuzz
 // use while loop to continue running until 100
 // use if else statement to find numbers
-// if number divisible by three then assign fizz and add to sum
+// if number divisible by three but not 5 (no duplicates) then assign fizz and add to sum
 // if number divisible by five then assign buzz and add to sum
 // if number is divisible by three && five then assign fizzbuzz and add to sum
+
+var number = 0;
+
+for (number = 0; number < 100; number ++){
+
+  if (number % 3 === 0){
+        console.log("fizz");
+  }
+  else if (number % 5 === 0) {
+        console.log("buzz");
+  }
+  else if ((number % 3 === 0) && (number % 5 === 0)) {
+        console.log("fizzBuzz");
+  }
+  else {
+        console.log(number);
+  }
+}
