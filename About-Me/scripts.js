8821108c6278855237dd7d4ef9982efa2b2008a8
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-09 16:48:52 -0500

    accessing span

---
+++ About-Me/scripts.js
@@ -0,0 +1,25 @@
+
+// change font family of body to sans serif font
+var font = document.querySelector("body");
+  font.style.fontFamily = "Arial";
+
+// replace each of the spans with your own information
+var fullName = document.getElementById("fullname");
+  fullName.textContent = "Nicky Cadavillo";
+
+var home = document.getElementById("hometown");
+  home.textContent = "New York";
+
+var favMovie = document.getElementById("movie");
+  favMovie.textContent = "The Hunger Games";
+
+var favFood = document.getElementById("food")
+
+
+// terate through each li and change the class to listitem.
+// Add a style tag that sets a rule for listitem to make the color red.
+
+
+
+// Create a new img element and set its src attribute to a picture of you.
+// Append that element to the page.
