cb2961d33a7a3a5adf3e1e6e4b620124f1dd2da7
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-10 09:54:25 -0500

    slow start to morning challenge

---
+++ daily-challenge-2-10.js
@@ -0,0 +1,15 @@
+// Write a function named calculateAge that:
+// Takes 2 arguments: birth year, current year
+// calculates the 2 possible ages based on those years
+// outputs the results to the screen like so: "You are either NN or NN"
+// Call the function 3 times with different sets of values
+
+//calculateAge
+//calculateAge - 1
+
+function calculateAge (birth year, current year) {
+
+}
+
+
+console.log("You are either " + "calculateAge" + " or" + "calculateAge - 1")
