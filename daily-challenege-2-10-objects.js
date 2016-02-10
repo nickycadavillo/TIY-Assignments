4b1a0d0a164f75711f7a2c8ffeb06c06f90df0e3
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-10 10:43:06 -0500

    attempt at objects

---
+++ daily-challenege-2-10-objects.js
@@ -0,0 +1,18 @@
+// Create an object to hold information on your fav recipe.
+//
+// It should have properties for title (string), servings (number), and ingredients (array of strings).
+//
+// One separate lines (one console.log statement for each), log the recipe information so that is looks like:
+
+
+
+var recipe = {
+    title: "mole",
+    serves: "serves: " + 2,
+    ingredients: ["cinnamon", "cumin", "cocoa"]
+}
+
+
+console.log(recipe.title);
+console.log(recipe.serves);
+console.log(recipe.ingredients);
