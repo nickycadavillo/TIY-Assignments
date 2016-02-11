ba7877a0abd5c027b6c0cc83dde617a3984a0f0e
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-11 10:07:31 -0500

    no time to finish!!!

---
+++ daily-challenge-2-11.js
@@ -0,0 +1,21 @@
+lifetime supply of your favorite snack: cookies
+
+write a function named calculateSupply that takes 2 arguments: age, amount per day,
+calculates the amount consumed for rest of the life (based on a constant max age)
+outputs the results to the screen like so:
+"You will need NN to last you until the ripe old age of X"
+Call that function three times, passing in different values each time
+
+
+var age = 90 //ripe age constant
+var amountPerDay = [5, 10, 15] //different values per day
+var total;
+
+
+function calculateSupply(age, amountPerDay){
+    if 
+}
+
+console.log("You will need " + " to last you until the ripe old age of" + age)
+console.log("You will need " + " to last you until the ripe old age of" + age)
+console.log("You will need " + " to last you until the ripe old age of" + age)
