cfa3858d4fe36abf4ffef83374aa46775a2ea7f7
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-17 10:32:49 -0500

    daily challenge

---
+++ daily-challenge-2-17.js
@@ -0,0 +1,22 @@
+// write a function named helloWorld that:
+// takes 1 argument, a language code (es, de, en)
+// returns "hello, world" for the given language, for at least 3 languages.
+// It should default to returning english
+// call that function for each of the support languages and log the result to make sure it works
+
+function helloWorld (nn){
+    if (nn === es) {
+        return "hola, mundo";
+    }
+    else if (nn === fr) {
+        return "bonjour, monde";
+
+    } else   (nn === en) {
+        return  "hellow, world";
+
+    }
+};
+
+console.log(hellowWorld(es));
+
+//syntax errors!!!
