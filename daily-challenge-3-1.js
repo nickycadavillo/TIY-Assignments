490ff3db526232a9d019e92c4932d84dcddd6fc5
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-01 10:39:55 -0500

    daily challenge broken code

---
+++ daily-challenge-3-1.js
@@ -0,0 +1,13 @@
+//write a function that takes a certain type of tag and counts the number of elements with that tag.
+// the function should return "there are x tags of type y on the page"
+
+//example:  countTags('p'): "there are 3 tags of type p on the page"
+//
+
+
+function countTags(p) {
+   var y = p.length
+    console.log("There are" + y + "tags of type" + p "on the page");
+}
+
+console.log(countTags());
