6042effc4f8b55b1094f039ce63a55fa43873d37
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-07 10:36:02 -0500

    pushing up what i have

---
+++ daily-challenge-3-7.js
@@ -0,0 +1,12 @@
+// have the function LetterCapitalize(srt) take the str parameter being passed and capitalize the first letter of each word. Words will be separated by only one space.
+
+
+function letterCapitalize(str){
+    //first make sure all words are lower case
+    return str.toLowerCase()
+    // then split? the string  and make first character uppercase...forgot how...
+
+
+}
+
+console.log(letterCapitalize('HELLO WORLD'))
