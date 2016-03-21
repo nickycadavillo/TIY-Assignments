a57888a30e4b616fe829ddec1e1db76b9d7f3bf0
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-21 10:40:09 -0400

    well, i got some pseudocode i guess

---
+++ daily-challenge-3-21.js
@@ -0,0 +1,26 @@
+//A palindromic number reads the same both ways.  The largest palindrome made from the product of
+//two 2-digit numbers is 9009 = 91 x 99
+
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+
+function palindrome(){
+
+    var numbers;
+
+    //iterate through all the numbers starting at 0
+
+    for (var i = 0; i < numbers.length ;i++){
+        function(a,b){
+            //starting value will be a and then a * b and so forth
+            //then find the product of those
+            //but the last three digits need to be the mirror the first three
+            //will need to use modulus?
+
+        }
+    }
+
+
+
+}
